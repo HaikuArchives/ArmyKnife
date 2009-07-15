674761508d28515be642b749332e618e91b280ee
scottmc <scottmc@67539fd6-432c-0410-ad19-9ddc0ca9f31c>
2009-07-15 07:12:15 +0000

    Removed stray FlipsideAE directories, added .OptionalPackageDescription file, adjusted making of zipfile packages to match OptionalPackage zip file naming format.
    git-svn-id: http://svn.osdrawer.net/armyknife@35 67539fd6-432c-0410-ad19-9ddc0ca9f31c

--- source/FlipSideAE/FlipSideAE/CheckMark.cpp
+++
@@ -1,8 +0,0 @@
-#include <be/support/Debug.h>
-#include "CheckMark.h"
-
-CheckMark::CheckMark() :
-	BBitmap(BRect(0,0,kImageWidth,kImageHeight),kImageColorSpace)
-{
-	SetBits(kImageBits,BitsLength(),0,kImageColorSpace);
-}
