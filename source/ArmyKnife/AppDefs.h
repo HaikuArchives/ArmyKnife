bd934a1d790ad98ba3ee8c67288ff59c19b35fb2
scottmc <scottmc@67539fd6-432c-0410-ad19-9ddc0ca9f31c>
2009-07-15 08:07:07 +0000

    Updated version and readme.html
    git-svn-id: http://svn.osdrawer.net/armyknife@36 67539fd6-432c-0410-ad19-9ddc0ca9f31c

--- source/ArmyKnife/AppDefs.h
+++ source/ArmyKnife/AppDefs.h
@@ -3,21 +3,21 @@
 
 #define AUTHOR "Jason Burgess"
 #define PROJECT "FlipSide Software"
-#define PROJECT_WWW "http://www.beunited.org"
+#define PROJECT_WWW "http://dev.osdrawer.net/projects/show/armyknife"
 #define MAINTAINER "Jonas Sundström"
 #define MAINTAINER_EMAIL "jonas@kirilla.com"
 
 #ifdef _TTE_
 #define APPLICATION "The Army Knife - TuneTracker Edition"
 #define APPLICATION_DIR "ArmyKnifeTTE"
-#define SIGNATURE "application/x-vnd.beunited.armyknife-tte"
+#define SIGNATURE "application/x-vnd.osdrawer.armyknife-tte"
 #else
 #define APPLICATION "The Army Knife"
 #define APPLICATION_DIR "ArmyKnife"
-#define SIGNATURE "application/x-vnd.beunited.armyknife"
+#define SIGNATURE "application/x-vnd.osdrawer.armyknife"
 #endif
 
-#define VERSION "4.0.0"
+#define VERSION "4.1.0"
 
 #define PROJECT_DIR "FlipSide Software"
 #define WINDOW_FILE "window"
